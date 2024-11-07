213aa60371d00bdae2ffcf0920f47967b8a4b830
Henry P Salazar <henry.salazar@sofka.com.co>
2024-11-07 10:31:10 -0500

    delete skipped test

--- tests/app/ui/components/Column.test.tsx
+++ tests/app/ui/components/Column.test.tsx
@@ -19,16 +19,6 @@ describe('Column component', () => {
     expect(cells).toHaveLength(4);
   });
 
-  it.skip('should display the correct value in each cell', () => {
-    render(<Column columnIndex={0} board={mockBoard} handleDrop={vi.fn()} />);
-
-    const cells = screen.getAllByRole('cell');
-    expect(cells[0]).toHaveTextContent(PLAYER.PLAYER1);
-    expect(cells[1]).toHaveTextContent(PLAYER.PLAYER2);
-    expect(cells[2]).toHaveTextContent(PLAYER.PLAYER1);
-    expect(cells[3]).toHaveTextContent(PLAYER.PLAYER2);
-  });
-
   it('should call handleDrop when clicked', () => {
     const handleDropMock = vi.fn();
     render(<Column columnIndex={1} board={mockBoard} handleDrop={handleDropMock} />);
